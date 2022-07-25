2733ae18f11ea4d6fe73b983a972018eeede754a
DinoChan <dino.chan@live.com>
2022-07-25 18:20:02 +0800

    update some code

---
+++ DesignAndAnimationLab/DesignAndAnimationLab/Demos/Gooey/ColorMatrixPipelineEffect.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Toolkit.Uwp.UI.Media;
+using Microsoft.Toolkit.Uwp.UI.Media.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Effects;
+
+namespace DesignAndAnimationLab.Demos.Gooey
+{
+    public sealed class ColorMatrixPipelineEffect : PipelineEffect
+    {
+        /// <inheritdoc/>
+        public override PipelineBuilder AppendToBuilder(PipelineBuilder builder)
+        {
+            Func<IGraphicsEffectSource, IGraphicsEffectSource> factory2 = (source) => new ColorMatrixEffect()
+            {
+                ColorMatrix = new Matrix5x4()
+                {
+                    M11 = 1,
+                    M12 = 0,
+                    M13 = 0,
+                    M14 = 0,
+                    M21 = 0,
+                    M22 = 1,
+                    M23 = 0,
+                    M24 = 0,
+                    M31 = 0,
+                    M32 = 0,
+                    M33 = 1,
+                    M34 = 0,
+                    M41 = 0,
+                    M42 = 0,
+                    M43 = 0,
+                    M44 = 18,
+                    M51 = 0,
+                    M52 = 0,
+                    M53 = 0,
+                    M54 = -7,
+                },
+                Source = source
+            };
+            return builder.Effect(factory2);
+        }
+    }
+}
