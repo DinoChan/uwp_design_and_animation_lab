39d091e1f7e6e52160aeb1fec5d7866a8f996dca
DinoChan <dino.chan@live.com>
2022-07-28 10:00:03 +0800

    update some code

---
+++ DesignAndAnimationLab/DesignAndAnimationLab/Common/DoubleProgresser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace DesignAndAnimationLab.Common
+{
+    public class DoubleProgresser
+    {
+        public DoubleProgresser(double seconds, bool autoReverse)
+        {
+            Duration = new Duration(TimeSpan.FromSeconds(seconds));
+            AutoReverse = autoReverse;
+        }
+
+        public EasingFunctionBase EasingFunction { get; set; }
+
+        public Duration Duration { get; set; } = new Duration(TimeSpan.FromSeconds(1));
+
+        public bool AutoReverse { get; set; }
+
+        public double GetCurrentProgress(TimeSpan timeSpan)
+        {
+            var scalingFactor = AutoReverse ? 2d : 1d;
+            var offsetFromBegin = timeSpan.Ticks % (Duration.TimeSpan.Ticks * scalingFactor) % Duration.TimeSpan.Ticks;
+            double progress = offsetFromBegin / Duration.TimeSpan.Ticks;
+
+            if (EasingFunction != null)
+                progress = EasingFunction.Ease(progress);
+
+            return progress;
+        }
+    }
+}
--- DesignAndAnimationLab/DesignAndAnimationLab/Demos/Gooey/GooeyEllipseDemoPage.xaml.cs
+++ DesignAndAnimationLab/DesignAndAnimationLab/Demos/Gooey/GooeyEllipseDemoPage.xaml.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using DesignAndAnimationLab.Common;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Effects;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
@@ -22,9 +29,86 @@ namespace DesignAndAnimationLab.Demos.Gooey
     /// </summary>
     public sealed partial class GooeyEllipseDemoPage : Page
     {
+        private GaussianBlurEffect _effect;
+        private ICanvasImage _image;
+        private Vector2 _canvasActualSize;
+        private DoubleProgresser _leftProgresser;
+        private DoubleProgresser _rightProgresser;
+
         public GooeyEllipseDemoPage()
         {
             this.InitializeComponent();
+
+            var visual = ElementCompositionPreview.GetElementVisual(this);
+            var compositor = visual.Compositor;
+            var containerVisual = compositor.CreateContainerVisual();
+
+            ElementCompositionPreview.SetElementChildVisual(Root, containerVisual);
+
+            _leftProgresser = new DoubleProgresser(4, true) { EasingFunction = new CircleEase { EasingMode = EasingMode.EaseOut } };
+            _rightProgresser = new DoubleProgresser(4, true) { EasingFunction = new CircleEase { EasingMode = EasingMode.EaseOut } };
+
+        }
+
+        private void OnCreateResource(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            var effect1 = new GaussianBlurEffect()
+            {
+                BlurAmount = 20f,
+            };
+
+            _effect = effect1;
+
+            var effect2 = new ColorMatrixEffect()
+            {
+                ColorMatrix = new Matrix5x4()
+                {
+                    M11 = 1,
+                    M12 = 0,
+                    M13 = 0,
+                    M14 = 0,
+                    M21 = 0,
+                    M22 = 1,
+                    M23 = 0,
+                    M24 = 0,
+                    M31 = 0,
+                    M32 = 0,
+                    M33 = 1,
+                    M34 = 0,
+                    M41 = 0,
+                    M42 = 0,
+                    M43 = 0,
+                    M44 = 18,
+                    M51 = 0,
+                    M52 = 0,
+                    M53 = 0,
+                    M54 = -7,
+                },
+                Source = effect1
+            };
+
+            _image = effect2;
+        }
+
+        private void OnDraw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args)
+        {
+            Windows.UI.Xaml.Media.Animation.clo
+            var source = new CanvasCommandList(sender);
+            using (var ds = source.CreateDrawingSession())
+            {
+                ds.FillCircle(_canvasActualSize / 2, 100, new CanvasSolidColorBrush(sender, Windows.UI.Colors.Black));
+                ds.FillCircle(_canvasActualSize / 2 + new Vector2(100, 0), 60, new CanvasSolidColorBrush(sender, Windows.UI.Colors.Blue));
+              
+              
+            }
+
+            _effect.Source = source;
+            args.DrawingSession.DrawImage(_image);
+        }
+
+        private void OnCanvasSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _canvasActualSize = Canvas.ActualSize;
         }
     }
 }
